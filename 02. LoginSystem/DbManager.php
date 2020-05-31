63c04a002b864c8827eb5f0c6bc1ea8fa1f2957c
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-05-31 10:28:20 +0300

    New lesson

---
+++ 02. LoginSystem/DbManager.php
@@ -0,0 +1,32 @@
+<?php
+class DB{
+
+    private $conn;
+
+    function __construct(){
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "loginsystem";
+        $this->conn = new mysqli($servername, $username, $password, $dbname);
+    }
+
+    function CheckEmail($email){
+        $sql = "SELECT * FROM `users` WHERE `email` = '$email'";
+        $result = $this->conn->query($sql);
+        if ($result->num_rows > 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    function Register($email, $password){
+        $sql = "INSERT INTO users (email, password) VALUES('$email', '$password')";
+        $this->conn->query($sql);
+    }
+ 
+    function __destruct(){
+        $this->conn->close();
+    }
+}
---
+++ 02. LoginSystem/index.php
@@ -0,0 +1,28 @@
+<?php
+require_once "./DbManager.php";
+$db = new DB();
+
+$message = "";
+if (!empty($_POST)){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
+    if ($password == $confirm_password){
+        if($db->CheckEmail($email)){
+            $db->Register($email, crypt($password, "mysupersaltstring345343t54t54g45"));
+            $message = "Register success";
+        }
+        else{
+            $message = "Thes email already exists.";
+        }
+    }
+    else{
+        $message = "Password does not match";
+    }
+}
+
+require_once "./views/header.html";
+require_once "./views/main.html";
+require_once "./views/footer.html";
+
+?>
